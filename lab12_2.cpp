ce5353da39650a1f6388954db1ca3b6faad0ade6
660610873-Pollapat <polapat_chan@cmu.ac.th>
2023-12-26 16:53:03 +0700

    lab12_2 complete

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
 using namespace std;
 
+int gcd(int x,int y);
+
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
 }
+
+int gcd(int x,int y){
+    if (x%y!=0) {gcd(y,x%y);}
+    else return y;
+}
+
+//z = y%x
+//while (gcd(x,y) = gcd(x, y%x) = gcd(x , z)
+//(gcd(25,15) = gcd(15, 25%15) = gcd(15, 10)
